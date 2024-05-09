7d09cef6242b823981b3a110b5d619084c1f32f9
Zain Kergaye <zain4utah@gmail.com>
2024-05-09 01:09:18 -0600

    Overwriting old repo

---
+++ README.md
@@ -0,0 +1,3 @@
+# New NixOS config 
+
+This is my new NixOS config for dev, and everything else. Using flakes and home-manager
--- home.nix
+++ home.nix
@@ -3,7 +3,7 @@
 {
 
   imports = [
-    ./user/sh.nix
+    ./user/shell/sh.nix
   ];
 
   # Home Manager needs a bit of information about you and the paths it should
