1f8fe515de70aca705eef926eafbe7532bba835b
Zain Kergaye <zain4utah@gmail.com>
2025-04-22 15:11:33 -0600

    Flake update! 😁

--- flake.nix
+++ flake.nix
@@ -32,10 +32,12 @@
     extra-substituters = [
       "https://nixpkgs-wayland.cachix.org"
       "https://nix-community.cachix.org"
+      "https://hyprland.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
   };
 
