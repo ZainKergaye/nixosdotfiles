af005edee8dc9685e5c892aa1b47bd98ba8478cf
Zain Kergaye <zain4utah@gmail.com>
2025-04-27 16:41:29 -0600

    Update: Cleaned up unused programs 😴

--- user/programs/programs.nix
+++ user/programs/programs.nix
@@ -38,7 +38,6 @@
     rawtherapee
 
     #skewl
-    quartus-prime-lite
     asciidoctor-with-extensions
     kicad
 
@@ -48,21 +47,10 @@
     cbonsai
 
     rpi-imager
-
-    # Productivity focusing
-    ianny
   ];
-
-  wayland.windowManager.hyprland.settings.exec-once = [ "${pkgs.ianny}/bin/ianny" ];
-
-  # Quartus prime variables
-  home.sessionVariables.LM_LICENSE_FILE = "/home/${config.variables.username}/.dotfiles/secrets/LR-214324_License.dat";
-  home.sessionVariables.NUM_PARALLEL_PROCESSORS = "4";
 
   nixpkgs.config.allowUnfreePredicate = pkg:
     builtins.elem (lib.getName pkg) [
       "osu-lazer-bin"
-      "quartus-prime-lite-unwrapped"
-      "quartus-prime-lite"
     ];
 }
